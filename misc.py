7c7b38ad6df6cfcbb33a782d150fa990948a206b
Felix Finkeldey <felix.finkeldey@tu-dortmund.de>
2021-01-27 10:51:17 +0100

    added cached download and md5 calculation

--- misc.py
+++ misc.py
@@ -1,9 +1,12 @@
 """Miscellaneous methods"""
 
 import os
+import hashlib
 import functools
+from pathlib import Path
 from tempfile import mkstemp
 from shutil import move
+import gdown
 
 
 def to_local_dir(filehandle):  # needs __file__ from caller
@@ -15,9 +18,41 @@ def gen_dirs(directories):
     for directory in directories:
         if not os.path.exists(directory):
             try:
-                os.makedirs(directory)
+                Path(directory).mkdir(parents=True)
             except OSError:
                 print(f"Error: Creation of directory {directory} failed.")
+
+def md5sum(filename, blocksize=65536):
+    """Generate md5 sum"""
+    hsh = hashlib.md5()
+    with open(filename, 'rb') as f_handle:
+        for block in iter(lambda: f_handle.read(blocksize), b''):
+            hsh.update(block)
+    return hsh.hexdigest()
+
+
+def cached_download(url, path, md5=None, quiet=False):
+    """
+    Download url in path using md5.
+
+    References:
+        - https://github.com/wkentaro/fcn
+    """
+    def check_md5(path, md5):
+        print('[{:s}] Checking md5 ({:s})'.format(path, md5))
+        return md5sum(path) == md5
+
+    if os.path.exists(path) and not md5:
+        print('[{:s}] File exists ({:s})'.format(path, md5sum(path)))
+    elif os.path.exists(path) and md5 and check_md5(path, md5):
+        pass
+    else:
+        dirpath = os.path.dirname(path)
+        if not os.path.exists(dirpath):
+            os.makedirs(dirpath)
+        gdown.download(url, path, quiet=quiet)
+
+    return path
 
 def replace_line_in_file(pathname, pattern, subst):
     """Replace pattern in line with subst"""
@@ -48,4 +83,3 @@ def lazy_property(function):
         return getattr(self, attribute)
 
     return decorator
-
